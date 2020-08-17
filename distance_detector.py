ed6e64a4b13c3e20f8d50c218531cde3e30ed04c
Gaurav <gauravsinha952@gmail.com>
2020-08-18 02:36:38 +0530

    ensure there are at least two people detections

--- distance_detector.py
+++ distance_detector.py
@@ -68,4 +68,47 @@ while True:
 	# distance
 	violate = set()
 
-	
+	# ensure there are *at least* two people detections (required in
+	# order to compute our pairwise distance maps)
+	if len(results) >= 2:
+		# extract all centroids from the results and compute the
+		# Euclidean distances between all pairs of the centroids
+		centroids = np.array([r[2] for r in results])
+		D = dist.cdist(centroids, centroids, metric="euclidean")
+
+		# loop over the upper triangular of the distance matrix
+		for i in range(0, D.shape[0]):
+			for j in range(i + 1, D.shape[1]):
+				if D[i, j] < config.MIN_DISTANCE:
+					violate.add(i)
+					violate.add(j)
+
+	for (i, (prob, bbox, centroid)) in enumerate(results):
+		(startX, startY, endX, endY) = bbox
+		(cX, cY) = centroid
+		color = (0, 255, 0)
+
+		if i in violate:
+			color = (0, 0, 255)
+
+		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+		cv2.circle(frame, (cX, cY), 5, color, 1)
+
+	text = "Social Distancing Violations: {}".format(len(violate))
+	cv2.putText(frame, text, (10, frame.shape[0] - 25),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 3)
+
+	if args["display"] > 0:
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+		if key == ord("q"):
+			break
+
+	if args["output"] != "" and writer is None:
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(args["output"], fourcc, 25,
+			(frame.shape[1], frame.shape[0]), True)
+
+	if writer is not None:
+		writer.write(frame)
